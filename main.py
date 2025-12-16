55d8bc1c857fd3751fd252046971cdd543fd0936
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2025-12-16 15:20:02 -0700

    Made adjustments to 'main.py' to pass lesson

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-pygame.init()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         log_state()
