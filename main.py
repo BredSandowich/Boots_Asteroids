b85182d5becaafd5cee8ca99a06a1baa3c6a1458
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2025-12-17 15:58:01 -0700

    Added rotational function to player on player.py and screen updates on main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
         
     while True:
@@ -21,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         
         dt = clock.tick(60)/1000
