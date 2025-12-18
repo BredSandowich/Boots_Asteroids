96dc0de8a4ccf1023c2fa0defd5298fe8d8fc4fa
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2025-12-18 14:08:43 -0700

    Added asteroidfield.py and asteroids.py to files and incorporated their functionality into circleshape.py and main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+import sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -14,24 +17,38 @@ def main():
             
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
         
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         updatable.update(dt)
+        
         for item in drawable:
             item.draw(screen)
+            
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+         
         #player.draw(screen)
         #player.update(dt)
         pygame.display.flip()
-        
         dt = clock.tick(60)/1000
+        
         
         #print(dt) # Used temporarily to test if the clock is working
 
